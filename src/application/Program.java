e989cb5e0704d9616a08e649ccb7be7c8efd3adf
Wagner Pires <wagnerpires@hotmail.com>
2019-02-27 21:46:50 -0300

    Incluído código de busca

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+        /*
 		System.out.print("Matriz dimension: ");
 		int n = sc.nextInt();
 		Integer[][] mat = new Integer[n][n];
@@ -33,6 +33,44 @@ public class Program {
 		}
 		System.out.println();
 		System.out.println("Negative numbers: " + count);
-		sc.close();
+		*/
+		
+		System.out.print("Lines: ");
+		int l = sc.nextInt();
+		System.out.print("Columns: ");
+		int c = sc.nextInt();
+
+		int[][] mat = new int[l][c];
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}		
+		
+		System.out.println();
+		System.out.print("Search number: ");
+		int x = sc.nextInt();
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == x) {
+					System.out.println("Position " + i + "," + j + ":");
+					if (j > 0) {
+						System.out.println("Left: " + mat[i][j-1]);
+					}
+					if (i > 0) {
+						System.out.println("Up: " + mat[i-1][j]);
+					}
+					if (j < mat[i].length - 1) {
+						System.out.println("Right: " + mat[i][j+1]);
+					}
+					if (i < mat.length - 1) {
+						System.out.println("Down: " + mat[i+1][j]);
+					}
+				}
+			}
+		}		
+ 	sc.close();
 	}
 }
